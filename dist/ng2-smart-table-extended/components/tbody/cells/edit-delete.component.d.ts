35508d1c5b927298128951db2562f3184b307642
Filip Ditrich <ditrich@visionarygraphics.cz>
2018-11-22 20:02:56 +0100

    edit-deleteClassFunction + returnable DataSet

---
+++ dist/ng2-smart-table-extended/components/tbody/cells/edit-delete.component.d.ts
@@ -0,0 +1,23 @@
+import { EventEmitter, OnChanges } from '@angular/core';
+import { Grid } from '../../../lib/grid';
+import { Row } from '../../../lib/data-set/row';
+import { DataSource } from '../../../lib/data-source/data-source';
+export declare class TbodyEditDeleteComponent implements OnChanges {
+    grid: Grid;
+    row: Row;
+    source: DataSource;
+    deleteConfirm: EventEmitter<any>;
+    editConfirm: EventEmitter<any>;
+    edit: EventEmitter<any>;
+    delete: EventEmitter<any>;
+    editRowSelect: EventEmitter<any>;
+    isActionEdit: boolean;
+    isActionDelete: boolean;
+    editRowButtonContent: any;
+    deleteRowButtonContent: any;
+    deleteRowClassFunction: Function;
+    editRowClassFunction: Function;
+    onEdit(event: any): void;
+    onDelete(event: any): void;
+    ngOnChanges(): void;
+}
